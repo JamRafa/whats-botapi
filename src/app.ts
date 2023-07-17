9e678c3a6ad442dd7eae2e4eeb3778995b2b9bd9
JamRafa <rafa.deandrade35@gmail.com>
2023-07-17 18:35:11 -0300

    primeira atualizacao

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import { Message } from 'venom-bot';
+import "dotenv/config.js";
+import express, { Request, Response } from "express";
+import Sender from "./venom";
+import cors from 'cors'
+const sender = new Sender()
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(cors<Request>());
+app.get('/status', (req: Request, res: Response) => {
+    return res.send({
+        qr_code: sender.qrCode,
+        connected: sender.isConnected
+    })
+}
+)
+app.post('/send', async (req: Request, res: Response) => {
+    const { number, message } = req.body
+    console.log(message)
+    try {
+        await sender.sendText(number, message)
+        return res.status(200).json()
+    } catch (error) {
+        res.status(500).json({ satus: "error", message: error })
+    }
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log('rodando')
+})
---
+++ src/venom.ts
@@ -0,0 +1,75 @@
+import { error } from 'console';
+import parsePhoneNumber, { isValidPhoneNumber } from 'libphonenumber-js';
+import { type } from 'os';
+import { start } from 'repl'
+import { create, Whatsapp, Message, SocketState } from "venom-bot";
+
+export type QrCode = {
+  base64Qr: string
+  attempts?: number
+}
+
+class Sender {
+
+  public client: Whatsapp;
+  private connected: boolean;
+  private qr: QrCode
+
+  get isConnected(): boolean {
+    return this.connected
+  }
+
+  get qrCode(): QrCode {
+    return this.qr
+  }
+
+
+  constructor() {
+    this.initialize()
+  }
+
+  async sendText(to: string, body: string) {
+
+
+    if (!isValidPhoneNumber(to, "BR")) {
+      throw new Error('esse numero nao é valido, para o Brasil')
+    }
+
+    let phoneNumber = parsePhoneNumber(to, "BR")?.format("E.164")
+      .replace("+", "") as string
+
+    phoneNumber = phoneNumber.includes("@c.us")
+      ? phoneNumber :
+      `${phoneNumber}@c.us`
+
+    await this.client.sendText(phoneNumber, body)
+  }
+
+  private initialize() {
+
+    const qr = (base64Qr: string) => {
+      this.qr = { base64Qr }
+    }
+
+    const status = (statusSession: string) => {
+
+      this.connected = ["isLogged", "qrReadSuccess", "chatsAvaliable"].includes(
+        statusSession
+      )
+    }
+
+    const start = (client: Whatsapp) => {
+      this.client = client
+      client.onStateChange((state) => {
+        this.connected = state === SocketState.CONNECTED
+      })
+
+    }
+
+    create("ws-sender", qr, status)
+      .then((client) => start(client))
+      .catch((error) => console.log(error))
+  }
+}
+
+export default Sender
